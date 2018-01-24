687e59b1f300f2ede579f128a247ee98b342ffc3
NDIAYEa <ndiayeastu1@gmail.com>
2018-01-24 15:46:38 +0100

    fichier Getting modifié

--- test-tp/test/unit/GettingStartedTest.js
+++ test-tp/test/unit/GettingStartedTest.js
@@ -16,34 +16,3 @@ describe("A suite is just a function", function() {
     expect(a).toBe(true);
   });
 });
-describe("Factorial", function(){
-    var testCases = [
-        {
-            n : 0,
-            factorial : 1
-        },
-        {
-            n : 1,
-            factorial : 1
-        },
-        {
-            n : 2,
-            factorial : 2
-        },
-        {
-            n : 3,
-            factorial : 6
-        },
-        {
-            n : 4,
-            factorial : 24
-        }
-    ];
-
-    testCases.forEach(function(testCase){
-        it("should returns " + testCase.factorial + " when n = " + testCase.n, function(){
-            var result = Util.factorial(testCase.n);
-            expect(result).toEqual(testCase.factorial);
-        })
-    });
-});
